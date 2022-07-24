4728b791cb5410ad355a1acf8184c5a85618acc1
Jared Shetler <jshetler7@gmail.com>
2022-07-24 15:20:59 -0500

    initial commit, setup complete

---
+++ src/client/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+
+
+const App = () => {
+
+
+	return (
+		<main className="container my-5">
+			<h1 className="text-primary text-center">Hello !</h1>
+		</main>
+	);
+};
+
+
+export default App;
---
+++ src/client/views/view-template.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Template = () => {
+    return(
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+
+export default Template;
